016d56bc6f30d7ec3db96ab8f344d4b78e08f51b
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-02-10 16:46:14 -0800

    Migrating to config file

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const configuration = require("../../configuration.json");
 
 module.exports = {
     name: "guildCreate",
@@ -9,7 +10,7 @@ module.exports = {
                 .setColor("#00ccff")
                 .setThumbnail(client.user.avatarURL())
                 .setTitle(`Thanks for adding ${client.user.username}.`)
-                .addField("Introduction", `\`${client.user.username}\`'s bot's default prefix is ${process.env.PREFIX} \nThe directory of commands can be found using 'help' (with the default prefix). To get help for a specific command, you can do \`help commandName\`\nThis bot's main perks are moderation, cryptocurrency data (Bitcoin, Litecoin, etc.), Discord Version Information, and much more.\nEnjoy using the bot.`)
+                .addField("Introduction", `\`${client.user.username}\`'s bot's default prefix is ${configuration['PREFIX']} \nThe directory of commands can be found using 'help' (with the default prefix). To get help for a specific command, you can do \`help commandName\`\nThis bot's main perks are moderation, cryptocurrency data (Bitcoin, Litecoin, etc.), Discord Version Information, and much more.\nEnjoy using the bot.`)
                 .addField("Questions/Support", `Join our [Support Server](https://discord.gg/nJTCzzF)`)
                 .addField("Other", `[Invite Me](https://discord.com/api/oauth2/authorize?client_id=483768001024491521&permissions=2147483639&scope=bot)`)
                 .setTimestamp();
