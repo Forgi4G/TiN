d0445e81963a03ced075ca59ff2d5c8e628a4e6c
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-02-10 16:48:37 -0800

    Migrating to config file

--- src/commands/info/about.js
+++ src/commands/info/about.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const messenger = require('../../local-frameworks/messenger.js');
+const configuration = require('../../configuration.json');
 
 module.exports = {
     name: "about",
@@ -12,9 +13,9 @@ module.exports = {
         const msgFrame = new messenger({ client: client, listener: message } );
         const embed = new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle(process.env.APP_NAME + " - " + "General")
+            .setTitle(configuration["APP_NAME"] + " - " + "General")
             .setAuthor(`About the bot:`)
-            .setDescription('A simple bot for moderation and information.')
+            .setDescription('A concise bot for moderation and information.')
             .setThumbnail(client.user.displayAvatarURL())
             .addField('Learn More:', `Use the \`help\` command for the command list \nView our policy [here](https://github.com/Forgi4G/TiN/blob/master/POLICY.md) \nGet our version information with the \`version\` command`, true)
             .addField('TiN', '[Invite Me](https://discord.com/api/oauth2/authorize?client_id=483768001024491521&permissions=2147483639&scope=bot) \n[Support Server](https://discord.gg/nJTCzzF) \n[Policy](https://github.com/Forgi4G/TiN/blob/master/POLICY.md)')
