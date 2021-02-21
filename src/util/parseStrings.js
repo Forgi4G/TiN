4347e6c578bc9a62d2d9e3b7698fc965c2c539e4
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-02-20 21:16:40 -0800

    Added capitalizeFirstChar() to set of parsing strings

--- src/util/parseStrings.js
+++ src/util/parseStrings.js
@@ -1,4 +1,6 @@
 //export const stripES: any = String.prototype.replace(/\s+/g, ' ').trim;
 module.exports = {
-    stripES: String.prototype.replace(/\s+/g, ' ').trim
+    stripES: String.prototype.replace(/\s+/g, ' ').trim,
+    capitalizeFirstChar: function capitalizeFirstLetter(str) { return str[0].toUpperCase() + str.slice(1); }
+
 }
