17a37fa9329a95e79c5386eeb57a44800fdc5d31
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-02-20 17:54:01 -0800

    No longer requiring an unnecessary function and using throwaway for unused args

--- src/commands/info/support.js
+++ src/commands/info/support.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-const { sendMessagePermCheck } = require('../../functions.js');
 const messenger = require('../../local-frameworks/messenger.js');
 const APP_NAME = JSON.parse(JSON.stringify((require("../../configuration.json"))))['APP_NAME'];
 
@@ -11,7 +10,7 @@ module.exports = {
     usage: "[command]",
     example: `support`,
     run: async (client, message, args) => {
-        const msgFrame = new messenger({ client: client, listener: message } );
+        const msgFrame = new messenger({ client: client, listener: message, throwaway: args } );
         const voteEmbed = new MessageEmbed()
             .setColor("#7289DA")
             .setDescription(`For questions, comments, concerns, feedback, help, additional inquiries, bug reports for ${APP_NAME}, consider joining our support server [here](https://discord.gg/nJTCzzF).`);
