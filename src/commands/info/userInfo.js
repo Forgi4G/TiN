05ce436a7c3ada1ed616d9eda7c2a8234dd94b75
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-03-05 15:03:45 -0800

    Revamped the whole command

--- src/commands/info/userInfo.js
+++ src/commands/info/userInfo.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const { getMember, formatDate } = require("../../util/functions.js");
+const { getMember, formatDate, getUserID } = require("../../util/functions.js");
 const { stripES } = require('../../util/parseStrings.js');
 
 const messenger = require('../../local-frameworks/messenger.js');
@@ -7,46 +7,94 @@ const messenger = require('../../local-frameworks/messenger.js');
 module.exports = {
     name: "userinfo",
     category: "info",
-    aliases: ["who", "user", "info", "whois"],
+    aliases: ["who", "user", "info", "whois", "ui"],
     description: "Returns user info for a user inside the server",
     usage: "[command] [userID / userMention]",
     example: `userinfo @Username#0001`,
-    run: (client, message, args) => {
+    run: async (client, message, args) => {
         const msgFrame = new messenger({ listener: message });
-        const member = getMember(message, args.join(" "));
+        let member = getMember(message, args.join(' '));
 
-        if (member === undefined) {
-            msgFrame.sendTempDefaultReplyConstr("The user you mentioned is either not in the server or doesn't exist.");
-        }
+        client.users.fetch(member ? member.user.id : args[0]).catch(error => {
+            if (error) return msgFrame.sendMessageConstr(`That is not a valid user.`);
+        });
 
-        if (member !== undefined) {
-            // Member variables
+        let userInfoEmbed = new MessageEmbed()
+            .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
+            .setTimestamp();
+
+        if (member) {
+            // Member roles and join dates
             const joined = formatDate(member.joinedAt);
             const roles = member.roles.cache
                 .filter(r => r.id !== message.guild.id)
                 .map(r => r).join(", ") || 'none';
 
-            // User variables
+            let getJoinDate = getDate(member.joinedAt);
+            let joinTime = `${Math.abs(getJoinDate.hours % 12)}:${getJoinDate.minutes} ${getJoinDate.AM_or_PM}`;
+
             const created = formatDate(member.user.createdAt);
 
-            const embed = new MessageEmbed()
-                .setFooter(member.displayName, member.user.displayAvatarURL())
+            let getCreatedDate = getDate(member.user.createdAt);
+            let createdTime = `${Math.abs(getCreatedDate.hours % 12)}:${getCreatedDate.minutes} ${getCreatedDate.AM_or_PM}`;
+            userInfoEmbed
                 .setThumbnail(member.user.displayAvatarURL())
-                .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+                .addField(`Username`, stripES.call(`${member.user.username}#${member.user.discriminator}`), true)
+                .addField(`ID`, stripES.call(`${member.user.id}`), true)
+                .addField(`Mention`, `<@${member.user.id}>`, true)
+                .setColor(member.user.displayHexColor === '#000000' ? '#ffffff' : member.user.displayHexColor)
+                .addField(`Account Creation Date`, `${created}\nat ${createdTime}`, true)
+                .addField(`Join Date`, `${joined}\nat ${joinTime}`, true)
+                .addField(`Nickname`, stripES.call(`${member.displayName === member.user.username ? "None" : member.displayName}`), true)
+                .addField(`Roles`, `${roles}`, true)
+                .addField(`Bot Account`, `${member.user.bot ? "True" : "False"}`)
+            if (member.presence.activities[0]) userInfoEmbed.addField(`Currently playing`, stripES.call(`${member.presence.activities[0].name}`));
+            return msgFrame.sendMessageConstr(userInfoEmbed);
+        } else if (!member && (await client.users.fetch(getUserID(args[0]))).id) {
+            // Get dates and user
+            let user = client.users.fetch(getUserID(args[0]));
 
-                .addField('Member information:', stripES.call(`**Display name:** ${member.displayName}
-                **Joined at:** ${joined}
-                **Roles:** ${roles}`), true)
+            let creationDate = formatDate(user.createdAt);
+            let createdDate = getDate((await user).createdAt);
+            let createdTime = `${Math.abs(createdDate.hours % 12)}:${createdDate.minutes} ${createdDate.AM_or_PM}`;
 
-                .addField('User information:', stripES.call(`**ID:** ${member.user.id}
-                **Username**: ${member.user.username}
-                **Tag**: ${member.user.tag}
-                **Created at**: ${created}`), true)
-                .setTimestamp()
-
-            if (member.presence.activity)
-              embed.addField('Currently playing', stripES.call(`**> Name:** ${member.presence.activity.name}`));
-            msgFrame.sendMessageConstr(embed);
+            userInfoEmbed
+                .setThumbnail((await user).displayAvatarURL())
+                .addField(`Username`, stripES.call(`${(await user).username}#${(await user).discriminator}`), true)
+                .addField(`ID`, stripES.call(`${(await user).id}`), true)
+                .addField(`Mention`, `<@${(await user).id}>`, true)
+                .setColor('#000000')
+                .addField(`Account Creation Date`, `${creationDate}\nat ${createdTime}`, true)
+                .addField(`Nickname`, `None`, true)
+                .addField(`Note`, `User is not in this server`, true)
+                .addField(`Bot Account`, `${(await user).bot ? "True" : "False"}`);
+            if ((await user).presence.activities[0]) userInfoEmbed.addField(`Currently playing`, stripES.call(`${(await user).presence.activities[0].name}`));
+            return msgFrame.sendMessageConstr(userInfoEmbed);
+        } else {
+            return msgFrame.sendMessageConstr(`That user does not exist.`);
         }
+    }
+}
+
+function getDate(arg) {
+    let date = new Date(arg);
+    let AM_or_PM;
+    if (date.getHours() >= 12 && date.getHours() < 24) {
+        AM_or_PM = "PM"
+    } else if (date.getHours() < 12 && date.getHours() > 0) {
+        AM_or_PM = "AM"
+    }
+
+    let minutes;
+    if (Math.abs(date.getMinutes()) < 10) {
+        minutes = "0" + String(date.getMinutes());
+    } else {
+        minutes = Math.abs(date.getMinutes());
+    }
+
+    return {
+        hours: date.getHours(),
+        minutes,
+        AM_or_PM
     }
 }
