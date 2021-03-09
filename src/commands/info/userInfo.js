51ca20ca6efa5d20c2c97211c81bf51b9f1fa812
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-03-08 21:29:34 -0800

    Large revamp and additional features to the user information command, users can now manage existed user info commands about them.

--- src/commands/info/userInfo.js
+++ src/commands/info/userInfo.js
@@ -2,6 +2,8 @@ const { MessageEmbed } = require("discord.js");
 const { getMember, formatDate, getUserID } = require("../../util/functions.js");
 const { stripES } = require('../../util/parseStrings.js');
 
+const { FLAGS } = require('../../data/user_flags.json');
+
 const messenger = require('../../local-frameworks/messenger.js');
 
 module.exports = {
@@ -15,12 +17,51 @@ module.exports = {
         const msgFrame = new messenger({ listener: message });
         let member = getMember(message, args.join(' '));
 
-        client.users.fetch(member ? member.user.id : args[0]).catch(error => {
-            if (error) return msgFrame.sendMessageConstr(`That is not a valid user.`);
-        });
+        if (args[0] && args[1]) {
+            message.deletable ? message.delete() : null;
+            if (args[0].toLowerCase() === "delete" && args[1]) {
+                let regArray;
+
+                try {
+                    regArray = (String(args[1]).match(/\d+/g));
+                } catch (e) {
+                    return msgFrame.sendTempDefaultMessageConstr(`Please enter a valid message ID or link.`);
+                }
+                if (regArray.length > 1) {
+                    let messageData = await getMessage({ channelID: regArray[1], messageID: regArray[2] });
+                    if (messageData.code !== 10008 && messageData.code !== 10003) {
+                        if ((await message.channel.messages.fetch(messageData.id)).embeds[0].fields[1].value === message.author.id) {
+                            (message.channel.messages.fetch(messageData.id)).then(m => m.delete());
+                        }
+                    } else {
+                        return msgFrame.sendTempDefaultMessageConstr(`You cannot delete other users' info messages or the link / ID you provided was invalid.`);
+                    }
+                } else if (regArray.length === 1) {
+                    let messageData = await getMessage( { channelID: message.channel.id, messageID: regArray[regArray.length - 1] });
+                    if (messageData.code !== 10008 && messageData.code !== 10003) {
+                        if ((await message.channel.messages.fetch(regArray[regArray.length - 1])).embeds[0].fields[1].value === message.author.id) {
+                            (message.channel.messages.fetch(regArray[regArray.length - 1])).then(m => m.delete());
+                        }
+                    } else {
+                        return msgFrame.sendTempDefaultMessageConstr(`You cannot delete other users' info messages or the link / ID you provided was invalid.`);
+                    }
+                }
+            }
+        }
+
+        if ((await getUser(getUserID(args[0]))).code) {
+            return msgFrame.sendTempDefaultMessageConstr(`That is not a valid user.`);
+        }
+
+        let userID;
+        if (!member && (await getUser(getUserID(args[0]))).code !== 10013) userID = getUserID(args[0]);
+        else if (!member && (await getUser(getUserID(args[0]))).code === 10013) {
+            userID = false;
+            return msgFrame.sendTempDefaultMessageConstr(`That is not a valid user.`);
+        }
 
         let userInfoEmbed = new MessageEmbed()
-            .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
+            //.setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
             .setTimestamp();
 
         if (member) {
@@ -48,11 +89,22 @@ module.exports = {
                 .addField(`Nickname`, stripES.call(`${member.displayName === member.user.username ? "None" : member.displayName}`), true)
                 .addField(`Roles`, `${roles}`, true)
                 .addField(`Bot Account`, `${member.user.bot ? "True" : "False"}`)
+            if ((await member.user).flags.toArray() && (await member.user).flags.toArray().length > 0) {
+                let flags = (await member.user).flags.toArray();
+                let flagsStr = "";
+                for (let i in flags) {
+                    if (!FLAGS[flags[i]]) flagsStr += "";
+                    else flagsStr += FLAGS[flags[i]] + " ";
+                }
+                if (flagsStr)
+                    userInfoEmbed
+                        .addField(`Flags`, `${flagsStr}`)
+            }
             if (member.presence.activities[0]) userInfoEmbed.addField(`Currently playing`, stripES.call(`${member.presence.activities[0].name}`));
             return msgFrame.sendMessageConstr(userInfoEmbed);
-        } else if (!member && (await client.users.fetch(getUserID(args[0]))).id) {
+        } else if (!member && userID) {
             // Get dates and user
-            let user = client.users.fetch(getUserID(args[0]));
+            let user = client.users.fetch(userID);
 
             let creationDate = formatDate(user.createdAt);
             let createdDate = getDate((await user).createdAt);
@@ -68,10 +120,19 @@ module.exports = {
                 .addField(`Nickname`, `None`, true)
                 .addField(`Note`, `User is not in this server`, true)
                 .addField(`Bot Account`, `${(await user).bot ? "True" : "False"}`);
+            if ((await user).flags.toArray() && (await user).flags.toArray().length > 0) {
+                let flags = (await user).flags.toArray();
+                let flagsStr = "";
+                for (let i in flags) {
+                    if (!FLAGS[flags[i]]) flagsStr += "";
+                    else flagsStr += FLAGS[flags[i]] + " ";
+                }
+                if (flagsStr)
+                    userInfoEmbed
+                        .addField(`Flags`, `${flagsStr}`)
+            }
             if ((await user).presence.activities[0]) userInfoEmbed.addField(`Currently playing`, stripES.call(`${(await user).presence.activities[0].name}`));
             return msgFrame.sendMessageConstr(userInfoEmbed);
-        } else {
-            return msgFrame.sendMessageConstr(`That user does not exist.`);
         }
     }
 }
@@ -97,4 +158,62 @@ function getDate(arg) {
         minutes,
         AM_or_PM
     }
+}
+
+async function getUser(id) {
+    let options = {
+        hostname: "canary.discord.com",
+        path: `/api/v8/users/${id}`,
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        },
+        method: "GET"
+    };
+    return new Promise((resolve, reject) => {
+        (require('https')).get(options, callback => {
+            let body = "";
+            callback.on("data", chunk => body += chunk);
+
+            callback.on("end", () => {
+                try {
+                    let endpointData = JSON.parse(body);
+                    resolve(endpointData);
+                } catch (e) {
+                    let error = {
+                        error: e
+                    }
+                    reject(error);
+                }
+            });
+        });
+    });
+}
+
+async function getMessage(options = {}) {
+    let reqOpts = {
+        hostname: "canary.discord.com",
+        path: `/api/v8/channels/${options.channelID}/messages/${options.messageID}`,
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        },
+        method: "GET"
+    }
+
+    return new Promise((resolve, reject) => {
+        (require('https')).get(reqOpts, callback => {
+            let body = "";
+            callback.on("data", chunk => body += chunk);
+            callback.on("end", () => {
+                try {
+                    let endpointData = JSON.parse(body);
+                    resolve(endpointData);
+                } catch (e) {
+                    let error = {
+                        error: e
+                    }
+                    reject(error);
+                }
+            });
+        });
+    });
 }
