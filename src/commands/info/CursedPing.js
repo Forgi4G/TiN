167253b2c2ec109481bcff92572470720966d2e4
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-03-19 23:09:07 -0700

    Cursed ping command for laughs

---
+++ src/commands/info/CursedPing.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "cursedping",
+    category: "info",
+    aliases: ["cursed"],
+    description: "Latency & API ping but written in the most cursed wave possible",
+    usage: "[command]",
+    example: `cursedping`,
+    run: async (client, message, args) => {
+        (function() { args.toUpperCase().toLowerCase(); })();
+        let ping = new MessageEmbed()
+            .setColor('B' + 'L' + 'A'.replace('A', 'U') + `R${PLE = "PLE"}`)
+            .addField(`Late n    c y`.replace(/ /g, "").trim(), `${Math['a' + 'B'.toLowerCase() + 's'](Date['no' + `w` + `lololololol`.replace("lololololol", "")]() - (message['c '.replace(' ', '')+'re'+'at'+'ed'+`timestamp`.substring(0, 1).toUpperCase()+`timestamp`.substr(1)]))}ms`, false + false === 0)
+            .addField(`A${"pi".toUpperCase()} Late ${"nnn".substr(2)}cy`.replace(/\s+/g, '')['trim'](), `${Math['r' + `${"ou".toUpperCase().toLowerCase().toString()}nd`](client["double u with a w".substr("double u with a w".length - 1) + "ssssssssssssss".substr("ssssssssssssss".length - 1)]['pinggggggg'.substring(0, 4)])}${'msmsms'.substr(4)}`, {} + [] === 0);
+        message['c' + 'h' + 'a' + 'n' + 'n' + `${el = "el"}`][`${"sending".substring(0, 4)}`](ping)['catch']((error => {{{{{{if(error) {{{{{{{{{{{}}}}}}}}}}}}}}}}}));
+    }
+}
