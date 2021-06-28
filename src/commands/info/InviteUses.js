f4351443e0923f66a8524f50a8b407fbba10160e
Forgi4G <42557352+Forgi4G@users.noreply.github.com>
2021-06-28 14:29:54 -0700

    Feature: Invite uses

---
+++ src/commands/info/InviteUses.js
@@ -0,0 +1,18 @@
+const messenger = require('../../local-frameworks/messenger.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "inviteuses",
+    category: "info",
+    aliases: ["invu", "invuses"],
+    description: "Gets number of uses of the first invite",
+    usage: "[command]",
+    example: `inviteuses`,
+    run: async (client, message, args) => {
+        const msgFrame = new messenger({ client: client, listener: message, throwaway: args } );
+        message.guild.fetchInvites().then(invites => {
+            const uses = invites.array().map(i => i.uses)[0];
+            return msgFrame.sendMessageConstr(`The most used invite has \`${uses}\` uses.`);
+        });
+    }
+};
